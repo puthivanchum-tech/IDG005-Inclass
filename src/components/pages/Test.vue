5d239bb57da01f63973fa3dc32da53eed14b75d5
puthivanchum <puthivanchum@gmail.com>
2026-06-17 11:44:01 +0700

    Module

--- src/components/pages/Test.vue
+++ src/components/pages/Test.vue
@@ -28,7 +28,7 @@
                   <th>Short Name</th>
                   <th>Created By</th>
                   <th>Updated By</th>
-                  <th>Actions <button class="btn btn-sm btn-primary">Add</button></th>
+                  <th>Actions <button class="btn btn-sm btn-primary" @click="showModal()">Add</button></th>
                 </tr>
               </thead>
               <tbody>
@@ -39,8 +39,8 @@
                   <td>{{ test.creator.name }}<br>{{ test.created_at }}</td>
                   <td>{{ test.updater.name }}<br>{{ test.updated_at }}</td>
                   <td>
-                    <button class="mx-1 btn btn-sm btn-info">View</button>
-                    <button class="mx-1 btn btn-sm btn-danger">Delete</button>
+                    <button class="mx-1 btn btn-sm btn-info" @click="viewTest(test.id)">View</button>
+                    <button class="mx-1 btn btn-sm btn-danger" @click="removeTest(test.id)">Delete</button>
                   </td>
                 </tr>
               </tbody>
@@ -50,15 +50,84 @@
       </div>
     </div>
   </div>
+  <div class="modal fade" id="TEST-MODAL" data-backdrop="static" data-keyboard="false" tabindex="-1">
+    <div class="modal-dialog modal-md">
+      <div class="modal-content">
+        <form @submit.prevent="saveTest()">
+          <div class="modal-header">
+            <h5 class="modal-title">Test Management</h5>
+            <button type="button" class="close" @click="hideModal()">
+              <span>×</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <label>Name (Khmer)</label>
+              <input v-model="testObj.name_kh" type="text" class="form-control"
+                :class="{ 'is-invalid': !!testErrObj.name_kh }">
+              <div class="invalid-feedback">
+                {{ testErrObj.name_kh }}
+              </div>
+            </div>
+            <div class="form-group">
+              <label>Name (English)</label>
+              <input v-model="testObj.name_en" type="text" class="form-control"
+                :class="{ 'is-invalid': !!testErrObj.name_en }">
+              <div class="invalid-feedback">
+                {{ testErrObj.name_en }}
+              </div>
+            </div>
+            <div class="form-group">
+              <label>Short Name (English)</label>
+              <input v-model="testObj.short_name" type="text" class="form-control"
+                :class="{ 'is-invalid': !!testErrObj.short_name }">
+              <div class="invalid-feedback">
+                {{ testErrObj.short_name }}
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer justify-content-between">
+            <button type="button" class="btn btn-secondary" @click="hideModal()">Cancel</button>
+            <button type="submit" class="btn btn-primary">Save</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
-import { ref, onMounted } from 'vue';
+import $ from 'jquery';
+import Swal from 'sweetalert2';
+import { ref, reactive, onMounted } from 'vue';
 import { apiGetTestsWithDetails } from '@/functions/api/test';
 import { CloseModal, LoadingModal, MessageModal } from "@/functions/swal";
 
 const tests = ref([]);
 
+const testObj = reactive({
+  id: null,
+  name_en: "",
+  name_kh: "",
+  short_name: "",
+});
+const testErrObj = reactive({
+  name_en: "",
+  name_kh: "",
+  short_name: "",
+});
+
+const defaultTestObj = JSON.parse(JSON.stringify(testObj));
+const defaultTestErrObj = JSON.parse(JSON.stringify(testErrObj));
+
+function resetAllState() {
+  Object.assign(testObj, defaultTestObj);
+  Object.assign(testErrObj, defaultTestErrObj);
+}
+
 onMounted(async () => {
+  $('#TEST-MODAL').on('hide.bs.modal', function () {
+    resetAllState();
+  });
   try {
     LoadingModal();
     await generateTests();
@@ -71,5 +140,32 @@ onMounted(async () => {
 async function generateTests() {
   const response = await apiGetTestsWithDetails();
   tests.value = response.data.tests;
+}
+async function saveTest() {
+  hideModal();
+}
+async function viewTest(id) {
+  showModal();
+}
+async function removeTest(id) {
+  Swal.fire({
+    title: 'Want to delete the test ?',
+    text: "Please make a confirmation.",
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#dc3545',
+    confirmButtonText: 'Yes, Delete it.'
+  }).then(async (sw) => {
+    if (sw.isConfirmed) {
+
+    }
+  });
+}
+
+function hideModal() {
+  $('#TEST-MODAL').modal('hide');
+}
+function showModal() {
+  $('#TEST-MODAL').modal('show');
 }
 </script>
